adb25e79ed4a16d0a3493c7e438d8b9576ac42b2
kelly-olsson <31006090+kelly-olsson@users.noreply.github.com>
2023-12-28 15:05:33 -0800

    1- Serialization step of Official Django REST Framework Tutorial

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 
 from tutorial.quickstart import views
 
+# Set up the router for users and groups
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -10,8 +11,15 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    # Include the router URLs
     path('', include(router.urls)),
+
+    # Include the Snippet app's URLs
+    path('', include('snippets.urls')),
+
+    # Include login URLs for the browsable API
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
-urlpatterns += router.urls
+# This line is no longer necessary as router.urls is already included in urlpatterns
+# urlpatterns += router.urls
